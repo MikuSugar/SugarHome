9d481138ed7a9303a121c06bf451254b9b5526e4
mikusugar <syfangjie@live.cn>
2022-02-12 14:43:59 +0800

    第一次提交

---
+++ src/main/java/me/mikusugar/webhome/APP.java
@@ -0,0 +1,55 @@
+package me.mikusugar.webhome;
+
+import com.alibaba.fastjson.JSONObject;
+import com.github.jknack.handlebars.internal.Files;
+import me.mikusugar.webhome.pojo.InputConfig;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author mikusugar
+ */
+public class APP {
+    public static void main(String[] args) throws IOException {
+
+        final InputConfig inputConfig = InputConfig.parseArgs(args);
+
+        System.out.println("读取的配置文件为:" + inputConfig);
+
+        String jsonStr = Files.read(new File(inputConfig.getJson()), StandardCharsets.UTF_8);
+
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        //模板所在目录，相对于当前classloader的classpath。
+        resolver.setPrefix("");
+        //模板文件后缀
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding("UTF-8");
+
+        TemplateEngine engine = new TemplateEngine();
+        engine.setTemplateResolver(resolver);
+        JSONObject jsonObject = JSONObject.parseObject(jsonStr);
+
+        //构造上下文(Model)
+        Context context = new Context();
+        context.setVariable("results", jsonObject);
+
+        //渲染模板
+        OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(inputConfig.getOut()), StandardCharsets.UTF_8);
+        System.out.println("writer = " + writer.getEncoding());
+
+        //这个example.html 放在resources 下面.
+        // 这样会生成一个result.html文件,结果都已经放进去了.
+        engine.process("example", context, writer);
+        writer.close();
+        engine.clearTemplateCache();
+
+
+    }
+}
---
+++ src/main/java/me/mikusugar/webhome/pojo/InputConfig.java
@@ -0,0 +1,57 @@
+package me.mikusugar.webhome.pojo;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * @author mikusugar
+ */
+public class InputConfig {
+
+    public static InputConfig parseArgs(String[] args) {
+        final HashMap<String, String> map = new HashMap<>();
+        for (String str : args) {
+            final String[] split = str.split("=");
+            map.put(split[0].trim(), split[1].trim());
+        }
+        return new InputConfig(map.get("json"), map.get("example"), map.get("out"));
+    }
+
+    public InputConfig(String json, String example, String out) {
+        this.json = json;
+        this.out = out;
+    }
+
+    public InputConfig() {
+    }
+
+
+    private String json;
+    private String out;
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+
+
+    public String getOut() {
+        return out;
+    }
+
+    public void setOut(String out) {
+        this.out = out;
+    }
+
+    @Override
+    public String toString() {
+        return "InputConfig{" +
+                "json='" + json + '\'' +
+                ", out='" + out + '\'' +
+                '}';
+    }
+}
